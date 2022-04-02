d304227c650af0efa3f947f67d952bf0b99fb039
Francisco Rivas <frivas@navteca.com>
2022-04-03 01:54:20 +0200

    Fixing more issues with udagram app

--- udagram-frontend/src/app/auth/auth-menu-button/auth-menu-button.component.ts
+++ udagram-frontend/src/app/auth/auth-menu-button/auth-menu-button.component.ts
@@ -7,42 +7,42 @@ import { AuthLoginComponent } from '../auth-login/auth-login.component';
 import { AuthRegisterComponent } from '../auth-register/auth-register.component';
 
 @Component({
-  selector: 'app-auth-menu-button',
-  templateUrl: './auth-menu-button.component.html',
-  styleUrls: ['./auth-menu-button.component.scss'],
+    selector: 'app-auth-menu-button',
+    templateUrl: './auth-menu-button.component.html',
+    styleUrls: ['./auth-menu-button.component.scss'],
 })
 export class AuthMenuButtonComponent implements OnInit {
 
-  constructor(
-    private auth: AuthService,
-    public modalController: ModalController
-    ) {}
+    constructor(
+        public auth: AuthService,
+        public modalController: ModalController
+    ) { }
 
-  async presentmodal(ev: any) {
-    const modal = await this.modalController.create({
-      component: AuthMenuUserComponent,
-    });
-    return await modal.present();
-  }
+    async presentmodal(ev: any) {
+        const modal = await this.modalController.create({
+            component: AuthMenuUserComponent,
+        });
+        return await modal.present();
+    }
 
-  async presentLogin(ev: any) {
-    const modal = await this.modalController.create({
-      component: AuthLoginComponent,
-    });
-    return await modal.present();
-  }
+    async presentLogin(ev: any) {
+        const modal = await this.modalController.create({
+            component: AuthLoginComponent,
+        });
+        return await modal.present();
+    }
 
-  async presentRegister(ev: any) {
-    const modal = await this.modalController.create({
-      component: AuthRegisterComponent,
-    });
-    return await modal.present();
-  }
+    async presentRegister(ev: any) {
+        const modal = await this.modalController.create({
+            component: AuthRegisterComponent,
+        });
+        return await modal.present();
+    }
 
-  logout() {
-    this.auth.logout();
-  }
+    logout() {
+        this.auth.logout();
+    }
 
-  ngOnInit() {}
+    ngOnInit() { }
 
 }
